f796450370be990f464da1edf26aa2f417ca178b
antho <anthonyyoussef01@gmail.com>
2024-02-15 17:21:05 -0500

    included the app.module.ts and updated the .gitignore

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+
+dotenv.config();
+
+console.log('PORT', process.env.PORT);
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT),
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
+})
+export class AppModule {}
