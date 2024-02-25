8e8fa8f77ddb982046bc5e8bca72b4d36f40e993
antho <anthonyyoussef01@gmail.com>
2024-02-25 17:40:35 -0500

    Added jsDOC comments to the countries controller

--- src/countries/controllers/countries/countries.controller.ts
+++ src/countries/controllers/countries/countries.controller.ts
@@ -6,11 +6,22 @@ import { Country } from '../../../typeorm/entities/country';
 export class CountriesController {
   constructor(private readonly countriesService: CountriesService) {}
 
+  /*
+   * This is a GET request to /countries that returns all countries
+   *
+   * @returns {Promise<Country[]>} - an array of Country objects
+   */
   @Get()
   async getAllCountries(): Promise<Country[]> {
     return this.countriesService.getAllCountries();
   }
 
+  /*
+   * This is a GET request to /countries/:countryCode that returns a country by its code
+   *
+   * @param {string} countryCode - the code of the country to return
+   * @returns {Promise<Country>} - the Country object with the specified code
+   */
   @Get(':countryCode')
   async getCountryByCode(
     @Param('countryCode') countryCode: string,
