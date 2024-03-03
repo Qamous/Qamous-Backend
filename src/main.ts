935b6264abf6fac6ee06411fc5f8139d60740e7f
antho <anthonyyoussef01@gmail.com>
2024-03-02 22:36:56 -0500

    XML Updates:
    - added a sitemap.xml
    - added it to the robots.txt

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as serveStatic from 'serve-static';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/robots.txt', serveStatic('utils/robots.txt'));
+  app.use('/sitemap.xml', serveStatic('utils/sitemap.xml'));
   await app.listen(3000);
 }
 bootstrap();
