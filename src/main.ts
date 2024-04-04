b4b1daa93fc67ccc4fa9a263495a34708420612e
antho <anthonyyoussef01@gmail.com>
2024-04-04 18:38:30 -0400

    CORS Update:
    - Using Cross-Origin Resource Sharing (CORS) in the backend to allow server requests from localhost:3001

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as serveStatic from 'serve-static';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/robots.txt', serveStatic('utils/robots.txt'));
   app.use('/sitemap.xml', serveStatic('utils/sitemap.xml'));
+  app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
   await app.listen(3000);
 }
 bootstrap();
