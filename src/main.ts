2af2c3f64b52eac7d5d70ebc26d7b354cc3f55fa
antho <anthonyyoussef01@gmail.com>
2024-04-15 18:25:29 -0400

    Express Session Updates: - Started working on Express Sessions in the backend

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import * as serveStatic from 'serve-static';
 import * as cors from 'cors';
 import process from 'process';
 import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // TODO: update robots.txt and sitemap.xml once we have a domain and we're ready to go live
   app.use('/robots.txt', serveStatic('utils/robots.txt'));
   app.use('/sitemap.xml', serveStatic('utils/sitemap.xml'));
   app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
@@ -17,9 +19,19 @@ async function bootstrap() {
       secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      },
     }),
   );
 
+  // Initialize and set up the Passport session middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
+
 bootstrap();
