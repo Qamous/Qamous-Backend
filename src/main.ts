0551551dfe69233c2dd402e574370f763a666014
antho <anthonyyoussef01@gmail.com>
2024-04-16 18:24:42 -0400

    Express-Session Updates:
    - Started implementing Express Sessions

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as serveStatic from 'serve-static';
 import * as cors from 'cors';
+import process from 'process';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/robots.txt', serveStatic('utils/robots.txt'));
   app.use('/sitemap.xml', serveStatic('utils/sitemap.xml'));
   app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+
+  // Set up the session middleware
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
